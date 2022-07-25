bc7fd03eb657a3f376f3674bacc140e836172103
Emre Dogan <emredogan@outlook.com>
2022-07-25 16:29:39 +0200

    Initial Commit

---
+++ SwiftUI_ScrollableList/SwiftUI_ScrollableListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ScrollableListApp.swift
+//  SwiftUI_ScrollableList
+//
+//  Created by Emre Dogan on 25/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ScrollableListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
